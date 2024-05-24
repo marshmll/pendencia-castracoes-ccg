8a1195f34202c844bbd4dbe771452bb86254aec8
VictorFadel06 <victorfadel96@gmail.com>
2024-05-24 00:20:40 -0300

    ajustado overview.js para calcular valores

--- assets/scripts/overview.js
+++ assets/scripts/overview.js
@@ -1,9 +1,15 @@
-let quantity = document.getElementById("valorVariavel");
-let pendentes = 0;
-let finished = 0;
-let males = 0;
-let females = 0;
-let valor = 0;
+let quantity = document.getElementById("valorQuantidade");
+let pendentes = document.getElementById("valorPendentes");
+let finished = document.getElementById("valorConcluídos");
+let males = document.getElementById("valorMachos");
+let females = document.getElementById("valorFemeas");
+
+let quantidade = 0;
+let sobrando = 0;
+let concluidos = 0;
+let machos = 0;
+let femeas = 0;
+
 
 
 
@@ -15,6 +21,7 @@ document.getElementById('add').addEventListener('click', function(event) {
   let raca = prompt("Informe a raça do gato: ");
   let castrado = prompt("Castrado? ");
 
+
   novoGato.innerHTML = `
   <img src="/assets/images/gato1.jpg" alt="foto" width="120" height="120" class="imagem">
   <div class="topicos">
@@ -24,12 +31,37 @@ document.getElementById('add').addEventListener('click', function(event) {
     <button class="remove">Remover</button>
   </div>
 `;
+
+  quantidade += 1;
+  sobrando +=1;
+  concluidos +=1;
+  machos +=1;
+  femeas +=1;
+
+
+
+
   novoGato.querySelector('.remove').addEventListener('click', function() {
-  novoGato.remove();
+    novoGato.remove();
+    quantidade -= 1;
+    sobrando -=1;
+    concluidos -=1;
+    machos -=1;
+    femeas -=1;
+
+    quantity.textContent = quantidade;
+    pendentes.textContent = sobrando;
+    finished.textContent = concluidos;
+    males.textContent = machos;
+    females.textContent = femeas;
   });
 
   document.getElementById('lista').appendChild(novoGato);
-  // valor += 1;
-  // quantity.innerHTML = valor;
+  
+  quantity.textContent = quantidade;
+  pendentes.textContent = sobrando;
+  finished.textContent = concluidos;
+  males.textContent = machos;
+  females.textContent = femeas;
 });
 
