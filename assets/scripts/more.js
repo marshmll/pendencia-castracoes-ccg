3798be6e5377a74a75c3457631085fa029980f05
Renan da Silva Oliveira Andrade <renan.silva3@pucpr.edu.br>
2024-06-05 10:45:11 -0300

    Finalização

---
+++ assets/scripts/more.js
@@ -0,0 +1,116 @@
+import client from "./util/client.js";
+
+const catId = window.location.href.split("?id=")[1];
+console.log(catId);
+
+async function setCatContent() {
+    let query = `
+    {
+      allCatRecords(
+        filter: {
+          id: { eq: "${catId}" }
+        }
+      ) {
+        id
+        image {
+          url
+        }
+        name
+        weight
+        race
+        color
+        sex
+        diseases
+        castrated
+        behavior
+        historic
+        attachments {
+          url
+        }
+        _status
+        _firstPublishedAt
+      }
+    
+      _allCatRecordsMeta {
+        count
+      }
+    }`;
+
+    let data = await client.queryCms(query);
+
+    console.log(data);
+
+    let imageUrl = data.allCatRecords[0].image
+        ? data.allCatRecords[0].image.url
+        : "https://i.pinimg.com/736x/53/e8/22/53e8223abe92162e99a04fffc67dbc70.jpg";
+    let name = data.allCatRecords[0].name;
+    let weight = data.allCatRecords[0].weight;
+    let race = data.allCatRecords[0].race;
+    let color = data.allCatRecords[0].color;
+    let sex = data.allCatRecords[0].sex == "female" ? "Fêmea" : "Macho";
+    let disease = data.allCatRecords[0].diseases
+        ? data.allCatRecords[0].diseases
+        : "Não há";
+    let historic = data.allCatRecords[0].historic
+        ? data.allCatRecords[0].historic
+        : "Não há";
+    let behavior = data.allCatRecords[0].behavior
+        ? data.allCatRecords[0].behavior
+        : "Não há";
+    let attachments = data.allCatRecords[0].attachments;
+
+    document.getElementById(
+        "cat-image"
+    ).style.backgroundImage = `url(${imageUrl})`;
+    document.getElementById("cat-name").append(name);
+    document.getElementById("cat-weight").append(weight);
+    document.getElementById("cat-race").append(race);
+    document.getElementById("cat-color").append(color);
+    document.getElementById("cat-sex").append(sex);
+    document.getElementById("cat-disease").append(disease);
+    document.getElementById("cat-historic").append(historic);
+    document.getElementById("cat-behavior").append(behavior);
+
+    let boxes = document.querySelectorAll(".attachments__box");
+
+    let i = 0;
+    attachments.forEach((att) => {
+        let link = document.createElement("a");
+        link.href = att.url;
+        link.innerText = "Ver anexo";
+        boxes[i].append(link);
+
+        i++;
+    });
+
+    document.querySelector(
+        ".buttons__button--edit"
+    ).href = `/pages/management.html?id=${catId}`;
+
+    document
+        .querySelector(".buttons__button--remove")
+        .addEventListener("click", async (e) => {
+            e.preventDefault();
+
+            try {
+                const response = await fetch(
+                    `https://ccgapi.vercel.app/api/content/delete/${catId}`
+                );
+
+                if (!response.ok) {
+                    throw new Error(
+                        `Erro ao fazer a requisição ao servidor: ${response.status}`
+                    );
+                }
+
+                window.location.pathname = "/pages/overview.html";
+
+                const responseJson = await response.json();
+                console.log(responseJson);
+            } catch (e) {
+                throw new Error(e);
+            }
+        });
+}
+
+setCatContent();
--- assets/scripts/overview.js
+++ assets/scripts/overview.js
@@ -1,4 +1,86 @@
+import client from "./util/client.js";
+
+let queryTotal = `
+    {
+        _allCatRecordsMeta{
+            count
+        }
+    }
+    `;
+
+let queryPending = `
+    {
+        _allCatRecordsMeta(
+            filter: {
+                castrated: { eq: false }
+            }
+        ) {
+            count
+        }
+    }
+    `;
+
+let queryConcluded = `
+    {
+        _allCatRecordsMeta(
+            filter: {
+                castrated: { eq: true }
+            }
+        ) {
+            count
+        }
+    }
+    `;
+
+let queryMale = `
+    {
+        _allCatRecordsMeta(
+            filter: {
+                sex: { eq: "male" }
+            }
+        ) {
+            count
+        }
+    }
+    `;
+
+let queryFemale = `
+    {
+        _allCatRecordsMeta(
+            filter: {
+                sex: { eq: "female" }
+            }
+        ) {
+            count
+        }
+    }
+    `;
+
 async function renderContent() {
+    let quantity = document.getElementById("valorQuantidade");
+    let pendentes = document.getElementById("valorPendentes");
+    let finished = document.getElementById("valorConcluídos");
+    let males = document.getElementById("valorMachos");
+    let females = document.getElementById("valorFemeas");
+
+    const totalCount = await client.queryCms(queryTotal);
+    const pendingCount = await client.queryCms(queryPending);
+    const concludedCount = await client.queryCms(queryConcluded);
+    const maleCount = await client.queryCms(queryMale);
+    const femaleCount = await client.queryCms(queryFemale);
+
+    console.log(totalCount);
+
+    quantity.innerText = totalCount._allCatRecordsMeta.count;
+
+    pendentes.innerText = pendingCount._allCatRecordsMeta.count;
+
+    finished.innerText = concludedCount._allCatRecordsMeta.count;
+
+    males.innerText = maleCount._allCatRecordsMeta.count;
+
+    females.innerText = femaleCount._allCatRecordsMeta.count;
+
     try {
         const response = await fetch(
             "https://ccgapi.vercel.app/api/content/all"
@@ -13,6 +95,8 @@ async function renderContent() {
         const responseJson = await response.json();
         console.log(responseJson);
 
+        document.getElementById("lista").innerHTML = "";
+
         responseJson.forEach((cat, index) => {
             console.log(cat);
 
@@ -21,39 +105,85 @@ async function renderContent() {
 
             novoGato.innerHTML = `
                 <img src="${
-                    cat.image ? cat.image.url : "https://i.pinimg.com/736x/53/e8/22/53e8223abe92162e99a04fffc67dbc70.jpg"
+                    cat.image
+                        ? cat.image.url
+                        : "https://i.pinimg.com/736x/53/e8/22/53e8223abe92162e99a04fffc67dbc70.jpg"
                 }" alt="alt" width="120" height="120" class="imagem">
                 <div class="topicos">
                     <p>Nome: ${cat.name}</p> 
                     <p>Raça: ${cat.race}</p>
                     <p>Castrado[S/N]? ${cat.castrated ? "Sim" : "Não"}</p>
                     <div class="botoes">
-                        <a class="remover" id="remover-${index}" data-id="${cat.id}">Remover</a>
-                        <a class="editar" id="editar" href="/pages/management.html?id=${cat.id}">Editar</a>
-                        <a class="visualizar" id="visualizar" href="/pages/more.html?id=${cat.id}">Visualizar</a>
+                        <a class="remover" id="remover-${index}" data-id="${
+                cat.id
+            }">Remover</a>
+                        <a class="editar" id="editar" href="/pages/management.html?id=${
+                            cat.id
+                        }">Editar</a>
+                        <a class="visualizar" id="visualizar" href="/pages/more.html?id=${
+                            cat.id
+                        }">Visualizar</a>
                     </div>
                 </div>
             `;
-
             document.getElementById("lista").appendChild(novoGato);
 
             // Adicionando event listener para cada botão de remover
-            document.getElementById(`remover-${index}`).addEventListener('click', async () => {
-                try {
-                    const delection = await fetch(`https://ccgapi.vercel.app/api/content/delete/${cat.id}`);
-                    
-                    if (delection.ok) {
-                        // Removendo o elemento da página
-                        document.getElementById(`gato-${index}`).remove();
-                        alert('Gato removido com sucesso da lista!')
-                    } else {
-                        // Exibindo um alert de erro caso a remoção não seja bem-sucedida
-                        alert('Erro ao remover o registro. Por favor, tente novamente.');
+            document
+                .getElementById(`remover-${index}`)
+                .addEventListener("click", async () => {
+                    try {
+                        const delection = await fetch(
+                            `https://ccgapi.vercel.app/api/content/delete/${cat.id}`
+                        );
+
+                        let quantity =
+                            document.getElementById("valorQuantidade");
+                        let pendentes =
+                            document.getElementById("valorPendentes");
+                        let finished =
+                            document.getElementById("valorConcluídos");
+                        let males = document.getElementById("valorMachos");
+                        let females = document.getElementById("valorFemeas");
+
+                        const totalCount = await client.queryCms(queryTotal);
+                        const pendingCount = await client.queryCms(
+                            queryPending
+                        );
+                        const concludedCount = await client.queryCms(
+                            queryConcluded
+                        );
+                        const maleCount = await client.queryCms(queryMale);
+                        const femaleCount = await client.queryCms(queryFemale);
+
+                        quantity.innerText =
+                            totalCount._allCatRecordsMeta.count;
+
+                        pendentes.innerText =
+                            pendingCount._allCatRecordsMeta.count;
+
+                        finished.innerText =
+                            concludedCount._allCatRecordsMeta.count;
+
+                        males.innerText = maleCount._allCatRecordsMeta.count;
+
+                        females.innerText =
+                            femaleCount._allCatRecordsMeta.count;
+
+                        if (delection.ok) {
+                            // Removendo o elemento da página
+                            document.getElementById(`gato-${index}`).remove();
+                            alert("Gato removido com sucesso da lista!");
+                        } else {
+                            // Exibindo um alert de erro caso a remoção não seja bem-sucedida
+                            alert(
+                                "Erro ao remover o registro. Por favor, tente novamente."
+                            );
+                        }
+                    } catch (error) {
+                        console.error("Erro ao remover o registro:", error);
                     }
-                } catch (error) {
-                    console.error('Erro ao remover o registro:', error);
-                }
-            });
+                });
         });
     } catch (e) {
         throw new Error(e);
@@ -61,8 +191,6 @@ async function renderContent() {
 }
 
 renderContent();
-
-
 
 // import client from "./util/client.js";
 
@@ -103,7 +231,7 @@ renderContent();
 //                     cat.image ? cat.image.url : "https://i.pinimg.com/736x/53/e8/22/53e8223abe92162e99a04fffc67dbc70.jpg"
 //                 }" alt="alt" width="120" height="120" class="imagem">
 // 				<div class="topicos">
-// 					<p>Nome: ${cat.name}</p> 
+// 					<p>Nome: ${cat.name}</p>
 // 					<p>Raça: ${cat.race}</p>
 // 					<p>Castrado[S/N]? ${cat.castrated ? "Sim" : "Não"}</p>
 // 					<div class="botoes">
